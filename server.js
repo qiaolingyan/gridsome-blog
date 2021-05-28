53e874efb927d67528412ebd0b8b0d61622bdf62
qiao <1379282164@qq.com>
2021-05-28 17:40:33 +0800

    'a'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 
-const port = 3000
+const port = 8080
 // 打包后的项目文件, 全部放在 public 目录中, public被视为网站根目录
 app.use('/',express.static('dist'));
 
